c899c855bb2806ea06ef7468863b4fbfe659598b
StackSpot Admin <admin@stackspot.com>
2022-03-03 17:16:27 +0000

    Update public repository

--- templates/lib/{{ computed_inputs.project_name }}-stack.ts
+++ templates/lib/{{ computed_inputs.project_name }}-stack.ts
@@ -9,18 +9,5 @@ class {{ computed_inputs.app_class_name }}Stack extends Stack {
     const eventBridge = new EventbridgeEnvJsiiComponent(this, '{{ computed_inputs.app_class_name }}Bus', {
       busName: '{{ inputs.bus_name }}',
     });
-
-    const rule = EventbridgeEnvJsiiComponent.addRule(
-        this,
-        eventBridge.bus.eventBusArn,
-        {
-          description: '{{ inputs.rule_description }}',
-          enabled: {% if inputs.rule_enabled %}true{% else %}false{% endif %},
-          ruleName: '{{ inputs.rule_name }}',
-          eventPattern: JSON.parse('{{ inputs.event_pattern }}'),
-        }
-    );
-
-    EventbridgeEnvJsiiComponent.addLambdaTarget(rule, destinedLambda);
   }
 }
